9d9a9861c296661a6c8cde531008d28fe7bef80c
Raul Cesar <raul.teixeira@gmail.com>
2012-02-02 23:42:16 -0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lancadespesas::Application.config.secret_token = '58ba1c668273ba284409f6b062ce7d5ef9751139fd4a47cbfe6a0d67b231c493ed3d90610c8bfc47b3936cfe2567cf56c76e98b5678b0b974e4c7f70ca13b2d4'
